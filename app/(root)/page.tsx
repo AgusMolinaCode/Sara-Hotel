dd6457fa0cdfe4729a03ac940e3a2d2232b04feb
Agustin Molina <am.motos@hotmail.com>
2024-05-18 00:39:26 -0300

    feat: Improve page layout and styling
    The code changes include:
    - Improved formatting and spacing in the page component
    - Updated image rendering with proper dimensions and styling
    - Updated font in the layout component to use Poppins
    
    These changes aim to enhance the visual appeal and user experience of the application.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,4 @@
-import {NavbarDemo} from "@/components/shared/Navbar";
+import { NavbarDemo } from "@/components/shared/Navbar";
 import Image from "next/image";
 import React from "react";
 
@@ -6,15 +6,23 @@ const page = () => {
   return (
     <div className="">
       <NavbarDemo />
-      <div className="">
+      <div className="relative">
         <Image
-            src="/images/cabaña1.jpg"
-            alt="hero"
-            layout="fill"
-          
-            
-            className=""
+          src="/images/cabaña1.jpg" // replace with your image path
+          width={1920}
+          height={1080}
+          className="max-h-[100vh] w-full object-cover"
+          alt="Background image"
         />
+        <div className="absolute inset-0 bg-gradient-to-r from-black via-black/55 to-transparent flex justify-start sm:pt-40 sm:px-20">
+          <div>
+            <h1 className="text-5xl max-w-xl text-white mb-4 font-bold">
+              Relájate en el Corazón de la{" "}
+              <span className="text-orange-400">Naturaleza</span>
+            </h1>
+            <h2 className="text-2xl max-w-lg text-gray-300">Escapadas perfectas para quienes buscan paz y tranquilidad, alejados del bullicio urbano, en un entorno natural sereno y revitalizante.</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/ui/navbar-menu.tsx
+++ components/ui/navbar-menu.tsx
@@ -70,7 +70,7 @@ export const Menu = ({
   return (
     <nav
       onMouseLeave={() => setActive(null)} // resets the state
-      className="relative rounded-full boder border-transparent backdrop-blur-xl bg-black/20 bg-white shadow-input flex justify-center space-x-4 px-8 py-6"
+      className="relative rounded-full boder border-transparent backdrop-blur-sm bg-black/50 shadow-input flex justify-center space-x-4 px-8 py-6"
     >
       {children}
     </nav>
