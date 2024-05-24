3e470d3ba75ca989de3f048327b84b4925fe7de4
Agustin Molina <am.motos@hotmail.com>
2024-05-23 23:41:17 -0300

    chore: Update dependencies and add form fields for RentForm

--- components/shared/RentForm.tsx
+++ components/shared/RentForm.tsx
@@ -2,7 +2,7 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { set, z } from "zod";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -13,10 +13,15 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { rentFormSchema } from "@/lib/validator";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
 
 export function RentForm() {
   // ...
@@ -36,28 +41,35 @@ export function RentForm() {
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="grid grid-cols-3 py-1">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="grid md:grid-cols-4 gap-1 py-[0.05rem] px-1"
+      >
         <FormField
           control={form.control}
           name="checkIn"
           render={({ field }) => (
             <FormItem className=" space-y-0">
               <FormControl>
-                <div className="grid w-full overflow-hidden rounded-full bg-grey-50 px-2 py-2">
+                <div className="grid w-full overflow-hidden rounded-lg bg-grey-50  py-2">
                   <DatePicker
-                    selected={field.value}
+                    selected={field.value as Date}
                     onChange={(date: Date) => {
                       form.setValue("checkIn", date);
+                      setStartDate(date);
                     }}
-                    showTimeSelect
-                    timeFormat="HH:mm"
-                    timeCaption="time"
-                    dateFormat="MMMM d, yyyy h:mm aa"
                     minDate={new Date()}
-                    className="w-full border border-black bg-slate-100/50 rounded-lg px-2 py-2 font-medium text-black"
-                    placeholderText="Seleccione la fecha de inicio"
+                    dateFormat="MMMM d, yyyy"
+                    selectsStart
+                    startDate={startDate}
+                    endDate={endDate}
+                    className="text-center border border-black bg-slate-100/50 rounded-lg px-2 py-2 font-medium text-black focus:outline-none"
                   />
                 </div>
               </FormControl>
@@ -67,23 +79,27 @@ export function RentForm() {
         />
         <FormField
           control={form.control}
-          name="checkIn"
+          name="checkOut"
           render={({ field }) => (
             <FormItem className=" space-y-0">
               <FormControl>
-                <div className="grid w-full overflow-hidden rounded-full bg-grey-50 px-2 py-2">
+                <div className="grid w-full overflow-hidden rounded-lg bg-grey-50 py-2">
                   <DatePicker
-                    selected={field.value}
+                    selected={field.value as Date}
                     onChange={(date: Date) => {
-                      form.setValue("checkIn", date);
+                      form.setValue("checkOut", date);
+                      setEndDate(date);
                     }}
-                    showTimeSelect
-                    timeFormat="HH:mm"
-                    timeCaption="time"
-                    dateFormat="MMMM d, yyyy h:mm aa"
-                    minDate={new Date()}
-                    className="w-full border border-black bg-slate-100/50 rounded-lg px-2 py-2 font-medium text-black"
-                    placeholderText="Seleccione la fecha de inicio"
+                    dateFormat="MMMM d, yyyy"
+                    selectsEnd
+                    startDate={startDate}
+                    endDate={endDate}
+                    minDate={
+                      new Date(
+                        new Date(startDate).setDate(startDate.getDate() + 1)
+                      )
+                    }
+                    className="text-center border border-black bg-slate-100/50 rounded-lg px-2 py-2 font-medium text-black focus:outline-none"
                   />
                 </div>
               </FormControl>
@@ -91,7 +107,15 @@ export function RentForm() {
             </FormItem>
           )}
         />
-        {/* <Button type="submit">Submit</Button> */}
+        <div className="space-y-0">
+          <Popover>
+            <PopoverTrigger className="w-full border border-black bg-slate-100/50 rounded-lg px-2 my-2 py-2 font-medium text-black">
+              Open
+            </PopoverTrigger>
+            <PopoverContent>Place content for the popover here.</PopoverContent>
+          </Popover>
+        </div>
+        <Button className="px-2 my-2 py-2" type="submit">Submit</Button>
       </form>
     </Form>
   );
