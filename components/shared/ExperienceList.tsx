4fd544dfe6c1c0fe4960f7ae6b12b44f6ba80185
Agustin Molina <am.motos@hotmail.com>
2024-06-02 02:14:56 -0300

    feat: Add layout grid demo to ExperienceList component

--- components/shared/ExperienceList.tsx
+++ components/shared/ExperienceList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { LayoutGridDemo } from "./LayoutGridDemo";
 
 const ExperienceList = () => {
   return (
@@ -9,6 +10,18 @@ const ExperienceList = () => {
       <h1 className="text-4xl text-black font-bold pt-8 border-b-4 border-orange-400 max-w-[18.8rem]">
         Our Experiences
       </h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-14">
+        <LayoutGridDemo/>
+        <div className="lg:col-span-1">
+          <h2 className="text-xl md:text-2xl font-semibold text-gray-600 mt-8">
+            All the experiences we offer
+          </h2>
+          <p className="text-lg text-gray-500 mt-2 mb-8">
+            Here you can find all the experiences we offer. Choose the one that
+            suits you best and enjoy your stay.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
